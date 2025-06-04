4ad8454a264685feaf7d0ff69758f632d7a3f0ff
pdmoreiraa <ph31383@gmail.com>
2025-06-04 08:03:27 -0300

    Criando as controllers

---
+++ Projeto2/Projeto2/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto2.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
