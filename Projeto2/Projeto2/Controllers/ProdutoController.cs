bc0dea81de4c976bf123458b64a9d975db8adde1
pdmoreiraa <ph31383@gmail.com>
2025-06-04 08:16:37 -0300

    Criando as controllers e métodos

--- Projeto2/Projeto2/Controllers/ProdutoController.cs
+++ Projeto2/Projeto2/Controllers/ProdutoController.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Projeto2.Models;
+using Projeto2.Repository;
 
 namespace Projeto2.Controllers
 {
     public class ProdutoController : Controller
     {
-        public IActionResult Index()
+        /* Declarando uma variável privada somente para leitura do  tipo UsuarioRepository chamada _usuarioRepository */
+
+        private readonly ProdutoRepository _produtoRepository;
+        /* Definindo o construtor da classe UsuarioController que vao receber uma instância de UsuarioRepository */
+
+        public ProdutoController(ProdutoRepository produtoRepository)
         {
+            _produtoRepository = produtoRepository;
+        }
+
+        [HttpGet]
+        public IActionResult CadastrarProduto()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CadastrarUsuario(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _produtoRepository.Cadastrar(produto);
+            }
             return View();
         }
     }
